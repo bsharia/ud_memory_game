82ade4c8fb1ebd4eb505c5743d04656ffd3f47d4
bsharia <trnsfrbfb01@gmail.com>
2023-01-08 20:09:48 +0100

    Toggle cards on click

--- js/app.js
+++ js/app.js
@@ -11,17 +11,30 @@ cards.sort(() => Math.random() - 0.5);
 // Dynamically Create a 2 x 2  div grid
 for (let i = 0; i < cards.length; i++) {
     let gCard = document.createElement("div");
-    let paraCon = document.createElement("p");
-  
+    // let paraCon = document.createElement("p");
+    gCard.className = "close";
+    // paraCon.className = "hidden";
     // Dynamically randomly set the content of each div to the  emoji
-    paraCon.textContent = cards[i];
-    gCard.appendChild(paraCon);
+    gCard.textContent = cards[i];
+    // gCard.appendChild(paraCon);
     board.appendChild(gCard);
 }
 
 
-// Add onClick listener to the board\
+// Add onClick listener to the board
 // Toggle classes on click
+board.addEventListener("click", toggleCard);
+
+function toggleCard(event) {
+    // console.log(event.target);
+    if(event.target.className === "close") {
+        event.target.className = "open";
+        // event.target.className = "shown";
+    } else if (event.target.className === "open") {
+        event.target.className = "close";
+        // event.target.className = "hidden";
+    }
+}
 
 // get the targeted click and store in a variable
 // listen for a second click and compare if the contents are equal
