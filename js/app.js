fadc643b145ce104d9cf3878db55483e2b525df1
bsharia <trnsfrbfb01@gmail.com>
2023-01-08 16:23:55 +0100

    dynamically create guess div

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 // Select the board
+const board = document.getElementById("gameBoard");
 
 // Create a list of emoji to use
+// const cards = ['🦄', '🐶', '🐱', '🦊', '🦝', '🐻'];
+const cards = ['🐶', '🐱', '🐶', '🐱'];
+
+// Randomize | shuffle card
+cards.sort(() => Math.random() - 0.5);
 
 // Dynamically Create a 2 x 2  div grid
+for (let i = 0; i < cards.length; i++){
+    const gCard = document.createElement("div");
+    gCard.className = "game-card";
+    gCard.textContent = cards[i];
+    board.appendChild(gCard);
+}
 
 // Dynamically randomly set the content of each div to the  emoji
 
