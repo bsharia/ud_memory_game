2645814f6c64aef9684ebbf6b7ae62aa1bf7460a
bsharia <trnsfrbfb01@gmail.com>
2023-01-08 16:04:51 +0100

    Add the pseudocode - logic for the app

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Select the board
+
+// Create a list of emoji to use
+
+// Dynamically Create a 2 x 2  div grid
+
+// Dynamically randomly set the content of each div to the  emoji
+
+// Add onClick listener to the board
+// get the targeted click and store in a variable
+// listen for a second click and compare if the contents are equal
+    // if equal, mark both div as done
+    // else toggle div back 
